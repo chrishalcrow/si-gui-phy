35f392a371a58e5c6fbcb38c0c97ae35debc60dc
chrishalcrow <chrishalcrow@gmail.com>
2025-07-27 16:47:31 +0100

    optimize and add ArgParser

--- sigui_from_phy.py
+++ sigui_from_phy.py
@@ -1,7 +1,15 @@
+from argparse import ArgumentParser
 from pathlib import Path
 from si_gui_phy.core import analyzer_from_phy
-phy_path = Path("/Users/christopherhalcrow/Work/Harry_Project/derivatives/M25/D20/sorter_output")
+from spikeinterface_gui import run_mainwindow
+
+parser = ArgumentParser()
+parser.add_argument('phy_path')
+phy_path = Path(parser.parse_args().phy_path)
+
+if phy_path.is_dir() is False:
+    raise FileExistsError(f"Path {phy_path} is not a folder.")
+
 sa = analyzer_from_phy(phy_path=phy_path)
 
-from spikeinterface_gui import run_mainwindow
 run_mainwindow(sa, mode="desktop", layout_preset="legacy")
--- src/si_gui_phy/core.py
+++ src/si_gui_phy/core.py
@@ -1,26 +1,24 @@
 import json
-from spikeinterface_gui import run_mainwindow
 import shutil
 from pathlib import Path
+
 import numpy as np
-
-from probeinterface import read_prb
-
-from spikeinterface.extractors import read_phy
-from spikeinterface.core import SortingAnalyzer, generate_ground_truth_recording, create_sorting_analyzer, ChannelSparsity
 import spikeinterface.qualitymetrics as siqm
+from numba import njit
+from probeinterface import read_prb
+from spikeinterface.core import (
+    ChannelSparsity,
+    SortingAnalyzer,
+    create_sorting_analyzer,
+    generate_ground_truth_recording,
+    set_global_job_kwargs,
+)
+from spikeinterface.extractors import read_phy
 
-extensions_to_compute = {
-    "unit_locations": {"method": "center_of_mass"},
-    "correlograms": {},
-    "template_similarity": {},
-    "isi_histograms": {},
-    "template_metrics": {}
-}
 
 def make_amplitudes(sa: SortingAnalyzer, phy_path: Path):
 
-    amps_folder = Path(sa.folder) / 'extensions' / 'spike_amplitudes'
+    amps_folder = Path(sa.folder) / "extensions" / "spike_amplitudes"
     amps_folder.mkdir(parents=True, exist_ok=True)
     shutil.copyfile(phy_path/"amplitudes.npy", amps_folder / "amplitudes.npy")
 
@@ -31,18 +29,22 @@ def make_amplitudes(sa: SortingAnalyzer, phy_path: Path):
     }
     make_params(amps_folder, params)
 
+
 def make_locations(sa, phy_path):
 
-    locs_folder: Path = sa.folder / 'extensions' / 'spike_locations'
+    locs_folder: Path = sa.folder / "extensions" / "spike_locations"
     locs_folder.mkdir(parents=True, exist_ok=True)
 
     locs_np = np.load(phy_path / "spike_positions.npy")
 
     num_dims = len(locs_np[0])
 
-    column_names = ['x', 'y', 'z'][:num_dims]
+    column_names = ["x", "y", "z"][:num_dims]
     dtype = [(name, locs_np.dtype) for name in column_names]
-    structured_array = np.array([tuple(row) for row in locs_np], dtype=dtype)
+
+    structured_array = np.array(np.zeros(len(locs_np)), dtype=dtype)
+    for a, column_name in enumerate(column_names):
+        structured_array[column_name] = locs_np[:,a]
 
     # Save as new .npy file
     np.save(locs_folder / "spike_locations.npy", structured_array)
@@ -52,56 +54,56 @@ def make_locations(sa, phy_path):
     params = {}
     make_params(locs_folder, params)
 
+
 def make_sparsity(sort, rec, phy_path):
 
     templates = np.load(phy_path / "templates.npy")
-    
+
     unit_ids = sort.unit_ids
     channel_ids = rec.channel_ids
 
     mask = np.sum(templates, axis=1) != 0
-    sparsity = ChannelSparsity(mask, unit_ids=unit_ids, channel_ids=channel_ids)
-    return sparsity
+    return ChannelSparsity(mask, unit_ids=unit_ids, channel_ids=channel_ids)
 
-def make_templates(sa, phy_path, unwhiten=True):
 
-    templates_folder = Path(sa.folder) / Path('extensions/templates')
+def make_templates(sa, phy_path, mask, unwhiten=True):
+
+    templates_folder = Path(sa.folder) / Path("extensions/templates")
     templates_folder.mkdir(parents=True, exist_ok=True)
 
     whitened_templates = np.load(phy_path / "templates.npy")
     wh_inv = np.load(phy_path / "whitening_mat_inv.npy")
 
-    if unwhiten:
-        new_templates = compute_unwhitened_templates(whitened_templates, wh_inv)
-    else:
-        new_templates = whitened_templates
+    new_templates = compute_unwhitened_templates(whitened_templates, wh_inv, mask) if unwhiten else whitened_templates
 
     np.save(templates_folder / "average.npy", new_templates)
 
     make_run_info(templates_folder)
 
     params = {
-        'operators': ['average'],
-        'ms_before': 1.0,
-        'ms_after': 1.06,
-        'peak_sign': "pos",
+        "operators": ["average"],
+        "ms_before": 1.0,
+        "ms_after": 1.06,
+        "peak_sign": "pos",
     }
     make_params(templates_folder, params)
 
 
-from numba import njit
-
 @njit()
-def compute_unwhitened_templates(whitened_templates, wh_inv):
- 
+def compute_unwhitened_templates(whitened_templates, wh_inv, mask):
+
     template_shape = np.shape(whitened_templates)
-    new_templates = np.zeros(template_shape) 
-    for a in range(template_shape[0]):
-        for b in range(template_shape[2]):
-            for c in range(template_shape[2]):
+    new_templates = np.zeros(template_shape)
+
+    sparsity_channel_ids = [np.arange(template_shape[-1])[unit_sparsity] for unit_sparsity in mask]
+
+    for a, unit_sparsity in enumerate(sparsity_channel_ids):
+        for b in unit_sparsity:
+            for c in unit_sparsity:
                 new_templates[a,:,b] += wh_inv[b,c]*whitened_templates[a,:,c]
-    
+
     return new_templates
+
 
 def make_run_info(folder):
 
@@ -109,58 +111,71 @@ def make_run_info(folder):
         "run_completed": True,
         "runtime_s": 0,
     }
-    with open(folder / 'run_info.json', 'w') as f:
+    with Path.open(folder / "run_info.json", "w") as f:
         json.dump(run_info, f)
+
 
 def make_params(folder, params):
 
-    with open(folder / 'params.json', 'w') as f:
+    with Path.open(folder / "params.json", "w") as f:
         json.dump(params, f)
 
 
-def analyzer_from_phy(phy_path, verbose=True) -> SortingAnalyzer:
+def analyzer_from_phy(phy_path) -> SortingAnalyzer:
+
+    set_global_job_kwargs(n_jobs=4)
 
     print("Verified that you have a phy folder made with KiloSort4!")
     print("Making a **recordingless** SortingAnalyzer")
     print("Reading the probe information...")
-    
+
+
     probe = read_prb(phy_path/ "probe.prb")
 
     print("Reading the sorting...")
 
     sort = read_phy(phy_path)
-    duration = sort.to_spike_vector()[-1][0]/sort.sampling_frequency
+
+    duration = sort._sorting_segments[0]._all_spikes[-1]/sort.sampling_frequency
     recording, _ = generate_ground_truth_recording(probe=probe.probes[0], sampling_frequency=30_000, durations=[duration])
 
     sparsity = make_sparsity(sort, recording, phy_path)
 
-    print("Creating a temporary folder...")
+    print("Creating a temporary analyzer...")
     sa = create_sorting_analyzer(sort,recording, format="binary_folder", folder="temp_analyzer", overwrite=True, sparse=True, sparsity=sparsity)
 
-    print("Copying and reformatting templates", end="")
+    print("Copying and reformatting templates", end="", flush=True)
     sa.compute("random_spikes")
-    make_templates(sa, phy_path, unwhiten=True)
-    print(", locations", end="")
+    make_templates(sa, phy_path, sparsity.mask, unwhiten=True)
+    print(", locations", end="", flush=True)
     make_locations(sa, phy_path)
-    print(" and ampltides...")
+    print(" and ampltides...", flush=True)
     make_amplitudes(sa, phy_path)
 
-    print("Computing unit locations, correlograms, template similarity, isi histograms and some template and quality metrics.")
-    sa.compute(extensions_to_compute)
+    print("Computing unit locations", flush=True, end="")
+    sa.compute("unit_locations", method="center_of_mass")
+
+    print(", correlograms", flush=True, end="")
+    sa.compute("correlograms")
+
+    print(", template_similarity", flush=True, end="")
+    sa.compute("template_similarity")
+
+    print(", isi_histograms", flush=True, end="")
+    sa.compute("isi_histograms")
+
+    print(", template_metrics", flush=True, end="")
+    sa.compute("template_metrics", include_multi_channel_metrics=True)
+
+    print(" and some quality metrics...", flush=True, end="")
+
     siqm.compute_firing_rates(sa)
     siqm.compute_presence_ratios(sa)
     siqm.compute_isi_violations(sa)
     siqm.compute_refrac_period_violations(sa)
-    siqm.compute_sliding_rp_violations(sa)
-    siqm.compute_amplitude_medians(sa)
-    siqm.compute_amplitude_cv_metrics(sa)
     siqm.compute_synchrony_metrics(sa)
     siqm.compute_firing_ranges(sa)
-    siqm.compute_drift_metrics(sa)
 
     #sa._recording = None
 
     return sa
-
-
-   
